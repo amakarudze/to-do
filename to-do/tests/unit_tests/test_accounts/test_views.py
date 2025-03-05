411bcbf6ea992dd70b916a547083ba8e84ea36e0
Anna Makarudze <anna@makarudze.com>
2025-03-05 13:15:35 +0100

    Add requirements.txt

--- to-do/tests/unit_tests/test_accounts/test_views.py
+++ to-do/tests/unit_tests/test_accounts/test_views.py
@@ -4,3 +4,7 @@ from django.urls import reverse
 def test_register_user(client):
     response = client.get(reverse("accounts:register"))
     assert response.status_code == 200
+
+
+def test_login(client):
+    pass
